5c55986848d775182089122459435e401ced961e
Irwanda Syafriadi <irwandasposu7@gmail.com>
2022-03-17 12:00:49 +0700

    feat: update page register

--- src/pages/auth/register.vue
+++ src/pages/auth/register.vue
@@ -3,60 +3,62 @@
 </script>
 
 <template>
-  <div class="container text-sm mt-0">
-    <div class="display-block  font:semibold text-3xl">
-      <p class="block mr-77">
-        Create
-      </p>
-      <p class="block mr-72">
-        Account
-      </p>
+  <div class="w-screen h-screen bg-white">
+    <div class="flex flex-col mb-4 pb-5">
+      <span class="pt-7 mt-3 font-bold text-4xl text-[#008CA3]">
+        Sign Up
+      </span>
+      <span class="mt-3 text-gray-400 font-semibold ">
+        Already have an account? <router-link to="/auth/login" class="text-[#008CA3] ml-1 font-semibold"> Sign In!</router-link>
+      </span>
     </div>
-
+    
     <!-- form  -->
-    <div class="container ">
-      <div class="flex flex-col px-3 py-3">
-        <label for="fullName" class="mr-80 mb-2">Your Name</label>
-        <input id="fullName" type="text" class="text-xs rounded bg-[#DDEDFA] border-0 focus:ring-0 border-blue-2 border-red-500 focus:border-blue-4 focus:border-red-500 caret-blue-400" placeholder="Full Name">
-      </div>
-      <div class="flex flex-col px-3 py-2">
-        <label for="email" class="mr-90 mb-2">Email</label>
-        <input id="email" type="email" class="text-xs rounded bg-blue-100 border-0 focus:ring-0 border-blue-2 border-red-500 focus:border-blue-4 focus:border-red-500 caret-blue-400 " placeholder="Your mail">
-      </div>
-      <div class="flex flex-col px-3 py-2">
-        <label for="username" class="mr-85 mb-2">Username</label>
-        <input id="username" type="text" class="text-xs rounded bg-blue-100 border-0 focus:ring-0 border-blue-2 border-red-500 focus:border-blue-4 focus:border-red-500 caret-blue-400 " placeholder="Username">
-      </div>
-
-      <div class="flex flex-col px-3 py-2">
-        <label for="password" class="mr-85 mb-2">Password</label>
-        <input id="password" type="password" class=" text-xs rounded bg-blue-100 border-0 focus:ring-0 border-blue-2 border-red-500 focus:border-blue-4 focus:border-red-500 caret-blue-400" placeholder="Your Secret Password">
-        <carbon-view id="password" class="icon-btn relative bottom-6 left-92" />
-      </div>
-      <div class="flex flex-col px-3 py-2">
-        <label for="retype" class="mr-70 mb-2 relative bottom-3">Retype Password</label>
-        <input id="retype" type="password" class="relative bottom-3 w-full text-xs rounded bg-blue-100 border-0 focus:ring-0 border-blue-2 border-red-500 focus:border-blue-4 focus:border-red-500 caret-blue-400" placeholder="Retype Your Secret password" required>
-        <carbon-view class="icon-btn relative bottom-9 left-92  " />
-      </div>
-      <div class="flex flex-col px-3 mt-0">
-        <label for="addres" class="mr-87 mb-2 relative bottom-5">Addres</label>
-        <input id="addres" type="text" class="relative bottom-5 text-xs rounded bg-blue-100  border-0 focus:ring-0 border-blue-2 border-red-500 focus:border-blue-4 focus:border-red-500 caret-blue-400" placeholder="Your adress">
-      </div>
+  <form class="space-y-7 pb-5">
+    <div class="">
+       <input type="text" name="full name" id="" class="w-110 rounded-md border-[#DBDFE3] focus:border-[#008CA3]" placeholder="Full Name (Based on KTP)">
     </div>
 
-    <div class="flex justify-start py-3">
-      <input id="terms" type="checkbox" class="ml-6 border-gray-800 focus:ring-0">
-      <label for="terms" class="ml-4 text-sm"><a href="#">i agree to the Terms & Conditions and Privacy Policy</a></label>
+    <div class="">
+       <input type="email" name="email" id="" class="w-110 rounded-md border-[#DBDFE3] focus:border-[#008CA3]" placeholder="Email Addres">
     </div>
-    <button id="btn" type="submit" class="py-4  bg-blue-500 p-3 rounded-md w-98 hover:bg-opacity-80 text-light-50">
-      Sign Up
-    </button>
 
-    <div class="flex flex-col justify-between mt-5">
-      <p class="text-gray-400">
-        Already have an account? <router-link to="/auth/login" class="py-4 text-blue-500 ml-3"> Sign In</router-link>
-      </p>
+    <div class="">
+       <input type="password" name="password" id="" class="w-110 rounded-md border-[#DBDFE3] focus:border-[#008CA3]" placeholder="Password">
+        <carbon-view id="password" class="icon-btn absolute -ml-7 mt-3" />
     </div>
+
+    <div class="">
+       <input type="password" name="confirm password" id="" class="w-110 rounded-md border-[#DBDFE3] focus:border-[#008CA3]" placeholder="Confirm Password">
+        <carbon-view id="password" class="icon-btn absolute -ml-7 mt-3" />
+    </div>
+
+    <div class="-ml-26">
+     <input type="checkbox" class="rounded-sm">
+      <label class="ml-2">
+       I agree to the 
+       <a href="#" class="text-[#008CA3]"> Term of Use </a>  and 
+       <a href="" class="text-[#008CA3]"> Privacy Policy </a>
+      </label>
+    </div>
+  </form>
+
+   <div>
+     <button class="w-110 py-3 rounded-md text-white bg-[#008CA3] shadow-xl active:shadow-2xl font-semibold">
+       Create My Account
+     </button>
+   </div>
+   
+   <div>
+      <p class="py-5 text-[#9695AD] font-semibold">OR</p>
+   </div>
+
+   <div>
+     <button class="flex flex-row justify-evenly  items-center w-110 ml-9 py-3 text-white rounded-md bg-[#4285F4] shadow-xl active:shadow-2xl">
+       <img src="/public/Google.png" class="h-6 rounded-sm -ml-30 bg-white"/>
+      <p class="-ml-10 font-semibold">Continue with Google </p> 
+     </button>
+   </div>
   </div>
 </template>
 
