4b9490c956a1843026589a8c0e1fde59cbeee9e2
iqbalnurhaq <iqbalnur833@gmail.com>
2022-07-19 13:41:10 +0700

    inbox page (notif page)

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,7 @@ module.exports = {
     "./layouts/**/*.vue",
     "./pages/**/*.vue",
     "./plugins/**/*.{js,ts}",
+    // "./nuxt.config.{js,ts}",
   ],
   darkMode: 'class', // or 'media' or 'class'
   presets: [],
